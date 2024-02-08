cded72eae81282d99ecdc0aa0d79a4e8c009504f
Martin Goldhahn <martin.goldhahn@lyse.no>
2024-02-08 15:13:20 +0100

    Use openapi-processor for code generation

--- resource-server/src/main/java/no/lyse/plattform/oauth2playground/resourceserver/web/ResourceController.java
+++ resource-server/src/main/java/no/lyse/plattform/oauth2playground/resourceserver/web/ResourceController.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import no.lyse.plattform.oauth2playground.resourceserver.api.Api;
 import no.lyse.plattform.oauth2playground.resourceserver.data.QuotesRepository;
 import no.lyse.plattform.oauth2playground.resourceserver.model.Quote;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.NotAcceptableStatusException;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -17,19 +21,26 @@ public class ResourceController implements Api {
     private final QuotesRepository quotes;
 
     @Override
-    public Mono<ResponseEntity<Mono<Quote>>> getQuote(String id, ServerWebExchange exchange) {
+    public Mono<Quote> getQuote(String id, ServerWebExchange exchange) {
         return quotes.getQuote(id)
-            .map(q -> ResponseEntity.ok(Mono.just(q)));
+            .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Element with id '%s' not found", id))));
+        // TODO: if we can generate Mono<ResponseEntity<Mono<Quote>>> instead of ResponseEntity<Mono<Quote>> we can use this code:
+//        return quotes.getQuote(id)
+//            .map(q -> ResponseEntity.ok(Mono.just(q)));
     }
 
     @Override
-    public Mono<ResponseEntity<Mono<Quote>>> getRandomQuote(ServerWebExchange exchange) {
-        return quotes.randomQuote()
-            .map(q -> ResponseEntity.ok(Mono.just(q)));
+    public Mono<Quote> getRandomQuote(ServerWebExchange exchange) {
+        return quotes.randomQuote();
+        // TODO: if we can generate Mono<ResponseEntity<Mono<Quote>>> instead of ResponseEntity<Mono<Quote>> we can use this code:
+//        return quotes.randomQuote()
+//            .map(q -> ResponseEntity.ok(Mono.just(q)));
     }
 
     @Override
-    public Mono<ResponseEntity<Flux<Quote>>> getQuotes(ServerWebExchange exchange) {
-        return Mono.just(ResponseEntity.ok(quotes.quotes()));
+    public Flux<Quote> getQuotes(ServerWebExchange exchange) {
+        return quotes.quotes();
+        // TODO: if we can generate Mono<ResponseEntity<Mono<Quote>>> instead of ResponseEntity<Mono<Quote>> we can use this code:
+//        return Mono.just(ResponseEntity.ok(quotes.quotes()));
     }
 }
